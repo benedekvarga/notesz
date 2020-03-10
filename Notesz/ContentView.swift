a58b8f4709f4655bae7cd3826a4bfc06a343f9d1
Benedek Varga <vargabenedek92@gmail.com>
2020-03-10 16:18:04 +0100

    Initial Commit

---
+++ Notesz/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Notesz
+//
+//  Created by Benedek Varga on 2020. 03. 10..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
